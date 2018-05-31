28781bd96bcefa678982bc546f3bd519a647b8f6
Jordan Joyner <jordanljoyner@gmail.com>
2018-05-31 12:13:58 -0400

    Adding the current state of the French Vanilla Game

---
+++ FrenchVanilla/Assets/Scripts/MasterRotationScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MasterRotationScript : MonoBehaviour {
+	public static float redRotation = 0.0f;
+	public static float blueRotation = 0.0f;
+	public static float greenRotation = 0.0f;
+	public Slider redSlider;
+	public Slider blueSlider;
+	public Slider greenSlider;
+	public GameObject objectPrefab;
+	public GameObject gridAnchor;
+	public Canvas baseCanvas;
+	public Text solvedText;
+	// Use this for initialization
+	void Start () {
+		Vector3 startPos = gridAnchor.transform.localPosition;
+		float xOffset = 150;
+		float yOffset = 150;
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 7; j++) {
+				Vector3 objPos = new Vector3 (startPos.x + j * xOffset, startPos.y - i * yOffset);
+				int index = 7 * i + j;
+				initRotationObject(RotationObjectScript.GameColor.RED, objPos, index);
+				initRotationObject(RotationObjectScript.GameColor.BLUE, objPos, index);
+				initRotationObject(RotationObjectScript.GameColor.GREEN, objPos, index);
+			}
+		}
+		redSlider.value = Random.Range (0.0f, 1.0f);
+		blueSlider.value = Random.Range (0.0f, 1.0f);
+		greenSlider.value = Random.Range (0.0f, 1.0f);
+	}
+
+	private void initRotationObject(RotationObjectScript.GameColor color, Vector3 pos, int index){
+		GameObject blueObject = (GameObject)Instantiate (objectPrefab);
+		blueObject.transform.SetParent (baseCanvas.transform);
+		blueObject.transform.localPosition = pos;
+		blueObject.GetComponent<RotationObjectScript> ().initObject (color, index);
+	}
+	float solvedOffset = 20.0f;
+	// Update is called once per frame
+	void Update () {
+		redRotation = 360.0f * redSlider.value;
+		blueRotation = 30.0f + 360.0f * blueSlider.value;
+		greenRotation = 90.0f + 360.0f * greenSlider.value;
+		if ((redRotation % 360.0f  < solvedOffset)
+			&& (blueRotation % 360.0f < solvedOffset)
+			&& (greenRotation % 360.0f < solvedOffset)) {
+			solvedText.gameObject.SetActive (true);
+		} else {
+			solvedText.gameObject.SetActive (false);
+		}
+	}
+}
---
+++ FrenchVanilla/Assets/Scripts/RotationObjectScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationObjectScript : MonoBehaviour {
+	public enum GameColor
+	{
+		RED =0,
+		BLUE,
+		GREEN
+	};
+	public GameColor gColor;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void initObject(GameColor color, int num){
+		gColor = color;
+		string redPrefix = "01_red_segments";
+		string bluePrefix = "02_blue_segments";
+		string greenPrefix = "03_green_segments";
+		string spriteName = "";
+		switch (color) {
+		case GameColor.RED:
+			spriteName = redPrefix;
+			break;
+		case GameColor.GREEN:
+			spriteName = greenPrefix;
+			break;
+		case GameColor.BLUE:
+			spriteName = bluePrefix;
+			break;
+		}
+		//spriteName += num.ToString ();
+		Debug.Log (spriteName);
+		Sprite[] sprites = Resources.LoadAll<Sprite>(spriteName);
+		this.GetComponent<SpriteRenderer> ().sprite = sprites [num];
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float rotValue = 0.0f;
+		switch (gColor) {
+		case GameColor.GREEN:
+			rotValue = MasterRotationScript.greenRotation;
+			break;
+		case GameColor.BLUE:
+			rotValue = MasterRotationScript.blueRotation;
+			break;
+		case GameColor.RED:
+			rotValue = MasterRotationScript.redRotation;
+			break;
+		}
+		transform.localRotation = Quaternion.Euler (0, 0, rotValue);
+	}
+}
